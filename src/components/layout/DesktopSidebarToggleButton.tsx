d8fc1c4481084ed6f40a995eacde55e2d6cae87b
Nghị <nghingothanhnghi@gmail.com>
2026-07-31 15:48:09 +0700

    update

--- src/components/layout/DesktopSidebarToggleButton.tsx
+++ src/components/layout/DesktopSidebarToggleButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IconLayoutSidebarLeftExpand, IconDeviceGamepad3, IconDeviceMobileCheck, IconCamera, IconBrain, IconPlant, IconUserShield, IconSportBillard, IconCheese, IconCalendarCheck, IconAnalyze, IconCashRegister, IconLogout, IconUserEdit } from '@tabler/icons-react';
+import { IconLayoutSidebarLeftExpand, IconDeviceGamepad3, IconDeviceMobileCheck, IconCamera, IconBrain, IconPlant, IconUserShield, IconSportBillard, IconCheese, IconArticle, IconCalendarCheck, IconAnalyze, IconCashRegister, IconLogout, IconUserEdit } from '@tabler/icons-react';
 import Button from '../common/Button';
 import DropdownButton from '../common/DropdownButton';
 import ListLink from '../common/ListLink';
@@ -48,6 +48,7 @@ const DesktopSidebarToggleButton: React.FC<DesktopSidebarToggleButtonProps> = ({
                 <ListLink to="/hydroponic-system" icon={<IconPlant size={16} />} label="Hydroponics" iconOnlyMode={true} />
                 <ListLink to="/jackpot" icon={<IconSportBillard size={16} />} label="Jackpot" iconOnlyMode={true} />
                 <ListLink to="/dashboard/products" icon={<IconCheese size={16} />} label="Products" iconOnlyMode={true} />
+                <ListLink to="/dashboard/cms" icon={<IconArticle size={16} />} label="CMS Content" iconOnlyMode={true} />
                 <ListLink to="/users" icon={<IconUserShield size={16} />} label="User Management" iconOnlyMode={true} />
                 <ListLink to="/migrate" icon={<IconAnalyze size={16} />} label="Data Migration" iconOnlyMode={true} />
                 <ListLink to="/payments" icon={<IconCashRegister size={16} />} label="Payments" iconOnlyMode={true} />
--- src/components/layout/SideMenu.tsx
+++ src/components/layout/SideMenu.tsx
@@ -1,5 +1,5 @@
 // src/components/layout/SideMenu.tsx
-import { IconDeviceMobileCheck, IconCamera, IconBrain, IconPlant, IconUserShield, IconSportBillard, IconCheese, IconCalendarCheck, IconAnalyze, IconCashRegister } from '@tabler/icons-react';
+import { IconDeviceMobileCheck, IconCamera, IconBrain, IconPlant, IconUserShield, IconSportBillard, IconCheese, IconArticle, IconCalendarCheck, IconAnalyze, IconCashRegister } from '@tabler/icons-react';
 import ListLink from '../common/ListLink';
 import { APP_NAME } from '../../config/constants';
 import Header from './Header';
@@ -44,6 +44,7 @@ export default function SideMenu({ open = false, onClose }: SideMenuProps) {
                         <ListLink to="/hydroponic-system" onClick={handleLinkClick} icon={<IconPlant size={16} />} label="Hydroponic System" />
                         <ListLink to="/jackpot" onClick={handleLinkClick} icon={<IconSportBillard size={16} />} label="Jackpot" />
                         <ListLink to="/dashboard/products" onClick={handleLinkClick} icon={<IconCheese size={16} />} label="Products" />
+                        <ListLink to="/dashboard/cms" onClick={handleLinkClick} icon={<IconArticle size={16} />} label="CMS Content" />
                         <ListLink to="/users" onClick={handleLinkClick} icon={<IconUserShield size={16} />} label="Users" />
                         <ListLink to="/migrate" onClick={handleLinkClick} icon={<IconAnalyze size={16} />} label="Data Migration" />
                         <ListLink to="/payments" onClick={handleLinkClick} icon={<IconCashRegister size={16} />} label="Payments" />
