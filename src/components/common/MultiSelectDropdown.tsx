83922f1307a5103eba4fc470171fafb8f2c2ee62
Nghị <nghingothanhnghi@gmail.com>
2026-08-02 00:06:00 +0700

    update

--- src/components/common/MultiSelectDropdown.tsx
+++ src/components/common/MultiSelectDropdown.tsx
@@ -92,7 +92,7 @@ const MultiSelectDropdown: React.FC<MultiSelectDropdownProps> = ({
 
     const dropdownContent = (
         <div
-            className="z-50 fixed w-64 max-h-72 overflow-y-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 dark:bg-gray-700 dark:text-gray-100 dark:border-gray-600 border"
+            className="z-[9999] fixed w-64 max-h-72 min-h-10 overflow-y-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 dark:bg-gray-700 dark:text-gray-100 dark:border-gray-600 border"
             style={{ top: position.top, left: position.left }}
             ref={dropdownRef}
         >
@@ -153,7 +153,8 @@ const MultiSelectDropdown: React.FC<MultiSelectDropdownProps> = ({
                     </div>
                 }
             />
-            {isOpen && dropdownContent}
+            
+            {isOpen && createPortal(dropdownContent, document.body)}
         </div>
     );
 };
