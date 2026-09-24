ad7a63bf7690a9fe5e7d87f7ad2bf15e5290257a
Nghị <nghingothanhnghi@gmail.com>
2026-09-24 12:47:22 +0700

    update

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -4,6 +4,10 @@ import i18n from '../i18n';
 
 export const useLanguage = () => {
   const changeLanguage = useCallback((lang: string) => {
+    // Save language for the backend 
+    document.cookie = `lang=${lang}; path=/; max-age=31536000`; 
+    
+    // Update frontend language
     i18n.changeLanguage(lang);
   }, []);
 
